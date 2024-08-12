921c9992fc3bc1e065c63e8e3cfdd777a9d4ce82
ibrahimsahin-dev <ibrahim.eray34@gmail.com>
2024-08-13 00:38:53 +0300

    çıkış problemi düzeltildi

--- proje.py
+++ proje.py
@@ -26,48 +26,60 @@ while(i):
  
     secim_oyun = input("Taş kağıt makas oyununu oynamak için 'Taş', 'Kağıt' veya 'Makas'dan 1'ini seçiniz:")    
     while(kazanan==0):#taş kağıt makas oyunu 
-        print("Bilgisayarın seçimi: ", bilgisayar_secimi_oyun)
         if secim_oyun == "Taş":
             if bilgisayar_secimi_oyun == "Taş":
+                print("Bilgisayarın seçimi: ", bilgisayar_secimi_oyun)
                 print("Berabere \n şimdi tekrar seçiniz.")
                 secim_oyun = input()
                 break
             elif bilgisayar_secimi_oyun == "Kağıt":
+                print("Bilgisayarın seçimi: ", bilgisayar_secimi_oyun)
                 print("Kaybettiniz \n şimdi bilgisayarın karakterini seçiyor.")
                 kazanan = -1
                 break
             elif bilgisayar_secimi_oyun == "Makas":
+                print("Bilgisayarın seçimi: ", bilgisayar_secimi_oyun)
                 print("Kazandınız \n şimdi karakterinizi seçebilirsiniz.")
                 kazanan = 1
                 break   
         elif secim_oyun == "Kağıt":
             if bilgisayar_secimi_oyun == "Taş":
+                print("Bilgisayarın seçimi: ", bilgisayar_secimi_oyun)
                 print("Kazandınız \n şimdi karakterinizi seçebilirsiniz.")
                 kazanan = 1
                 break
             elif bilgisayar_secimi_oyun == "Kağıt":
+                print("Bilgisayarın seçimi: ", bilgisayar_secimi_oyun)
                 print("Berabere \n şimdi tekrar seçiniz.")
                 secim_oyun = input()
             elif bilgisayar_secimi_oyun == "Makas":
+                print("Bilgisayarın seçimi: ", bilgisayar_secimi_oyun)
                 print("Kaybettiniz \n şimdi bilgisayarın karakterini seçiyor.")
                 kazanan = -1
                 break
         elif secim_oyun == "Makas":
             if bilgisayar_secimi_oyun == "Taş":
+                print("Bilgisayarın seçimi: ", bilgisayar_secimi_oyun)
                 print("Kaybettiniz \n şimdi bilgisayarın karakterini seçiyor.")
                 kazanan = -1
                 break
             elif bilgisayar_secimi_oyun == "Kağıt":
+                print("Bilgisayarın seçimi: ", bilgisayar_secimi_oyun)
                 print("Kazandınız \n şimdi karakterinizi seçebilirsiniz.")
                 kazanan = 1
                 break
             elif bilgisayar_secimi_oyun == "Makas":
+                print("Bilgisayarın seçimi: ", bilgisayar_secimi_oyun)
                 print("Berabere \n şimdi tekrar seçiniz.")
                 secim_oyun = input()
-        else:
-            secim_oyun == "q"
+        elif secim_oyun == "q":
             i = 0
             break
+        else:
+            print("Hatalı giriş yaptınız. Lütfen tekrar deneyiniz.")
+            secim_oyun = input()
+    if(i==0):
+        break
     print("Mevcut karakterler\n")
     for karakter in mevcut_karakterler:
         print(karakter)
@@ -94,5 +106,42 @@ while(i):
             print("Ejderha karakteri seçildi.")
             print("Ejderha karakterinin canı: ", ejderha)
             print("Ejderha karakterinin silahları: ", "Ateş baskını", "Rüzgar", "Işın gözler")
+    elif (kazanan==-1):
+        print("Bilgisayarın karakteri seçiliyor.")
+        bilgisayar_secimi_oyun = random.choice(mevcut_karakterler)
+        print("Bilgisayarın karakteri seçildi.")
+        print("Bilgisayarın karakterinin canı: ", bilgisayar_secimi_oyun)
+        mevcut_karakterler.remove(bilgisayar_secimi_oyun)
+        print("Mevcut karakterler\n")
+        for karakter in mevcut_karakterler:
+            print(karakter)
+        print("Karakterinizi seçiniz.")
+        karakter_secimi = input()
+        while(karakter_secimi not in mevcut_karakterler):
+    
+            if karakter_secimi == "Dev":
+                print("Dev karakteri seçildi.")
+                print("Dev karakterinin canı: ", dev)
+                print("Dev karakterinin silahları: ", "Demir Yumruk", "Balta", "Balyoz")
+            elif karakter_secimi == "buyucu":
+                print("buyucu karakteri seçildi.")
+                print("buyucu karakterinin canı: ", buyucu)
+                print("buyucu karakterinin silahları: ", "Ateş küre", "Can içeçeği", "Buz oku")
+            elif karakter_secimi == "Şovalye":
+                print("Şovalye karakteri seçildi.")
+                print("Şovalye karakterinin canı: ", sovalye)
+                print("Şovalye karakterinin silahları: ", "Kralın Kılıçı", "Topuz", "Kalkan")
+            elif karakter_secimi == "Ejderha":
+                print("Ejderha karakteri seçildi.")
+                print("Ejderha karakterinin canı: ", ejderha)
+                print("Ejderha karakterinin silahları: ", "Ateş baskını", "Rüzgar", "Işın gözler")
+            else:
+                print("Hatalı giriş yaptınız.")
+                karakter_secimi = input()
+    print("\n\n\n\n ############# Savaş başlıyor ############# \n")
+
+                
+
+    
         
     
